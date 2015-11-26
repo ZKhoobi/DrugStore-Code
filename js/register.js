2cd68efcc9c1f10acfca0eb6043185a03cf150d0
NioushaAtR <niousha.a1993@yahoo.com>
2015-11-26 23:29:04 +0330

    sign up compeletely works!

--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿/* $(document).ready(function () {
     $("#registeration").click(function () {
             var request = new XMLHttpRequest();
             var url = 'registeration.php';
@@ -12,16 +12,53 @@
 			
 			
     });
+}); */
+$(document).ready(function(){
+	
+	$('#regForm').submit(function(e) {
+
+		register();
+		e.preventDefault();
+		
+	});
+	
 });
 
-function processData(e){
-	var currentReadyState = e.target.readyState;
-	var currentStatus = e.target.status;
-	// alert(e.target.responseText);
-	if(currentReadyState == 4 && currentStatus == 200) {
-		alert(this.responseText);
-		
-	}
 
-		
+function register()
+{
+	hideshow('loading',1);
+	error(0);
+	$.ajax({
+		type: "POST",
+		url: "registeration.php",
+		data: $('#regForm').serialize(),
+		dataType: "json",
+		success: function(msg){
+			
+			if(parseInt(msg.status)==1)
+			{
+				window.location=msg.txt;
+			}
+			else if(parseInt(msg.status)==0)
+			{
+				error(1,msg.txt);
+			}
+			
+			hideshow('loading',0);
+		}
+	});
+}
+
+
+function hideshow(el,act)
+{
+	if(act) $('#'+el).css('visibility','visible');
+	else $('#'+el).css('visibility','hidden');
+}
+
+function error(act,txt)
+{
+	hideshow('error',act);
+	if(txt) $('#error').html(txt);
 }
--- registeration.php
+++ registeration.php
@@ -11,6 +11,7 @@ mysql_query("SET CHARACTER SET 'utf8'", $connection);
 mysql_query("SET character_set_connection = 'utf8'", $connection);
 
 mysql_select_db($database);
+
 function NewUser(){ 
 	$address = '';
 	$name = $_POST['rname']; 
@@ -19,35 +20,38 @@ function NewUser(){
 	$userName = $_POST['runame']; 
 	$password = $_POST['rpass']; 
 	$phoneNumber = $_POST['rnumber'];
-	$query = "INSERT INTO member (memberID,userName,password,email,name,familyName,phoneNumber,address) VALUES ('','$userName','$password','$email','$name','$familyName','$phoneNumber','$address')"; 
+	$active = '0';
+	$query = "INSERT INTO member (memberID,active,userName,password,email,name,familyName,phoneNumber,address) VALUES ('',$active,'$userName','$password','$email','$name','$familyName','$phoneNumber','$address')"; 
 	$data = mysql_query ($query)or die(mysql_error()); 
 	if($data){ 
-		echo "ثبت نام با موفقیت انجام شد.";
-		//echo json_encode(1);  
+		die(msg(0,"ثبت نام با موفقیت انجام شد."));
 	} 
 } 
 function SignUp(){ 
 	if(empty($_POST['rname']) || empty($_POST['rfname']) || empty($_POST['runame']) || empty($_POST['rpass']) || empty($_POST['rrpass']) || empty($_POST['rnumber'])|| empty($_POST['remail'])){
-		echo "لطفا ابتدا فرم را تکمیل کرده سپس دکمه ثبت نام را فشار دهید.";
-		//echo json_encode(2);
+		die(msg(0,"لطفا ابتدا فرم را تکمیل کرده سپس دکمه ثبت نام را فشار دهید."));
 	}
-	else if(($_POST['rpass'])!= ($_POST['rrpass'])){
-		echo "لطفا رمز عبور را دوباره وارد کنید.";
-		//echo json_encode(3);
+	if(!(preg_match("/^[\.A-z0-9_\-\+]+[@][A-z0-9_\-]+([.][A-z0-9_\-]+)+[A-z]{1,4}$/", $_POST['remail']))){
+		die(msg(0,"آدرس ایمیل نامعتبر است."));
 	}
-	else if(!empty($_POST['runame']) ){ //checking the 'user' name which is from Sign-Up.html, is it empty or have some text 
+	if(($_POST['rpass'])!= ($_POST['rrpass'])){
+		die(msg(0,"لطفا رمز عبور را دوباره وارد کنید."));
+	}
+	
+	if(!empty($_POST['runame']) ){ //checking the 'user' name which is from Sign-Up.html, is it empty or have some text 
 		$query = mysql_query("SELECT * FROM member WHERE userName = '$_POST[runame]' ") or die(mysql_error());
 		if(!$row = mysql_fetch_array($query)){
 			NewUser(); 
 		} 
-		else{ 
-			echo "این نام کاربری قبلا استفاده شده است."; 
-			//echo json_encode(4);
+		else{  
+			die(msg(0,"این نام کاربری قبلا استفاده شده است."));
 		} 
 	}
 } 
-if(isset($_POST['registeration'])){ 
-	SignUp(); 
+SignUp(); 
+function msg($status,$txt)
+{
+	return '{"status":'.$status.',"txt":"'.$txt.'"}';
 } 
 ?>
 
