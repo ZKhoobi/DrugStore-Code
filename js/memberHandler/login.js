90ecb06b5fd14489decf9be275a530acbcbd4ad0
NioushaAtR <niousha.a1993@yahoo.com>
2015-12-01 20:20:48 +0330

    layerd arch

---
+++ js/memberHandler/login.js
@@ -0,0 +1,64 @@
+﻿var login=0;
+$(document).ready(function(){
+	$("#myBtn").click(function () {
+		if(login==0)
+		{
+			$("#myModal").modal();
+			$('#lgForm').submit(function(e) {
+			lgin();
+			e.preventDefault();	
+			});
+		}
+		if(login==1)
+		{
+			
+		}
+	});
+	
+});
+
+
+function lgin()
+{
+	hideshow('loading',1);
+	error(0);
+	$.ajax({
+		type: "POST",
+		url: "login.php",
+		data: $('#lgForm').serialize(),
+		dataType: "json",
+		success: function(msg){
+			if(parseInt(msg.status)==1)
+			{
+				login=1;
+				success(0,msg.txt);
+			}
+			else if(parseInt(msg.status)==0)
+			{
+				
+				error(1,msg.txt);
+			}
+			
+			hideshow('loading',0);
+		}
+	});
+}
+
+
+function hideshow(el,act)
+{
+	if(act) $('#'+el).css('visibility','visible');
+	else $('#'+el).css('visibility','hidden');
+}
+
+function error(act,txt)
+{
+	hideshow('error',act);
+	if(txt) $('#error').html(txt);
+}
+function success(act,txt)
+{
+	$("#myModal").modal('hide');
+	if(txt) $('#myBtn').html('خروج');
+	if(txt) $('#lgname').html(txt);
+}
---
+++ memberHandler/login.php
@@ -0,0 +1,40 @@
+﻿<?php
+$host="localhost";
+$user="root";
+$password="";
+$database="drugstore";
+$connection=mysql_connect($host,$user,$password);
+
+mysql_query("SET NAMES 'utf8'", $connection);
+mysql_query("SET CHARACTER SET 'utf8'", $connection);
+mysql_query("SET character_set_connection = 'utf8'", $connection);
+
+mysql_select_db($database);
+//if(isset($_POST['lgin'])){
+if(empty($_POST['usrname']) || empty($_POST['psw'])){
+	die(msg(0,"لطفا نام کاربری و رمز عبور را وارد کنید."));
+}
+else{
+	$query = mysql_query("SELECT * FROM member WHERE userName = '$_POST[usrname]' AND password = '$_POST[psw]' AND active = 0 ") or die(mysql_error());
+	$row = mysql_fetch_array($query);
+	if($row){
+		session_start();
+		$name = $row["name"];
+		$familyName = $row["familyName"];
+		$_SESSION['name']=$name;
+		$_SESSION['family']=$familyName;
+		$_SESSION['id']=$row['memberID'];
+		$query = mysql_query("UPDATE member SET active = '1' WHERE userName = '$_POST[usrname]' AND password = '$_POST[psw]' ") or die(mysql_error());
+		$temp = $name . $familyName;
+		die(msg(1, $temp));
+	}
+	else{
+		die(msg(0,"نام کاربری یا رمز عبور اشتباه است."));
+	}
+}
+//}
+function msg($status,$txt)
+{
+	return '{"status":'.$status.',"txt":"'.$txt.'"}';
+}
+?>
